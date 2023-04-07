f756957ba35ac2aa9a32b676094b25f5bdcf7403
SEBASTIAN LANGHAMMER <sebastian.langhammer@mercedes-benz.com>
2023-04-07 16:08:14 +0200

    wip

--- learn.sh
+++ learn.sh
@@ -2,6 +2,7 @@
 folder="/home/azureuser/v2/rnn_handwriting_generation/res"
 for i in {1..200}
 do
+
   python3.8 train.py --folder $folder --batch_size 250 --learning_rate 0.002 --epoch $i
   python3.8 sample.py --folder $folder --epoch $i
 done
